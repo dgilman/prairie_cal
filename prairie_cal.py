35eb291e5b11e12ecb114e95eff02b75f6a9816c
David Gilman <davidgilman1@gmail.com>
2012-09-15 01:14:59 -0500

    Licensing typos

--- prairie_cal.py
+++ prairie_cal.py
@@ -5,18 +5,18 @@
 #
 # This file is part of prairie_cal.
 #
-# atom_maker is free software: you can redistribute it and/or modify
+# prairie_cal is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# atom_maker is distributed in the hope that it will be useful,
+# prairie_cal is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with atom_maker.  If not, see <http://www.gnu.org/licenses/>.
+# along with prairie_cal.  If not, see <http://www.gnu.org/licenses/>.
 
 from lxml import html
 import datetime
